f767185f7b5117c19b3697129ebfca1b664702eb
resq-u <jakub12lepianka@wp.pl>
2025-11-22 21:27:25 +0100

    Refactor App state structure, add dnd-kit and integrate CvEditor/CvPreview

--- src/App.jsx
+++ src/App.jsx
@@ -1,152 +1,246 @@
 import { useState } from "react";
-// import { Stage } from "./components/Stage.jsx"
-import { SectionsContainer } from "./components/SectionsContainer.jsx";
-// import './App.css'
+import { DndContext, closestCorners } from "@dnd-kit/core";
+import { arrayMove } from "@dnd-kit/sortable";
+import { CvPreview } from "./components/CvPreview.jsx";
+import { CvEditor } from "./components/CvEditor.jsx";
+import { initialData } from "./initialData.js";
 
 function App() {
-  const [personalData, setPersonalData] = useState([
-    {
-      key: crypto.randomUUID(),
-      firstName: "",
-      lastName: "",
-      email: "",
-      phoneNum: "",
-      birthDate: "",
-      city: "",
-    },
-  ]);
-  const [educationData, setEducationData] = useState([
-    {
-      key: crypto.randomUUID(),
-      school: "",
-      degree: "",
-      startDate: "",
-      endDate: "",
-      isCurrent: false,
-      city: "",
-      description: "",
-    },
-  ]);
-  const [employmentData, setEmploymentData] = useState([
-    {
-      key: crypto.randomUUID(),
-      jobTitle: "",
-      employer: "",
-      startDate: "",
-      endDate: "",
-      isCurrent: false,
-      city: "",
-      description: "",
-    },
-  ]);
+  const initialDataCopy = JSON.parse(JSON.stringify(initialData));
+  const [inputData, setInputData] = useState(initialDataCopy);
   const [sectionId, setSectionId] = useState(0);
+  const [isEdit, setIsEdit] = useState(true);
+  const [activeId, setActiveId] = useState(null);
 
   function handleChange(e) {
-    const name = e.target.name;
+    let name = e.target.name;
+    if (name.includes("-")) {
+      name = name.split("-")[0];
+    }
     const value =
       e.target.type === "checkbox" ? e.target.checked : e.target.value;
     const id = e.target.getAttribute("data-objid");
-    if (sectionId === 0) {
-      setPersonalData((prev) =>
-        setNewData({
-          currState: prev,
-          nameToSet: name,
-          valueToSet: value,
-          idToSet: id,
-        })
-      );
-    } else if (sectionId === 1) {
-      setEducationData((prev) =>
-        setNewData({
-          currState: prev,
-          nameToSet: name,
-          valueToSet: value,
-          idToSet: id,
-        })
-      );
-    } else if (sectionId === 2) {
-      setEmploymentData((prev) =>
-        setNewData({
-          currState: prev,
-          nameToSet: name,
-          valueToSet: value,
-          idToSet: id,
-        })
-      );
-    }
+    setInputData((prev) =>
+      setNewData({
+        currState: prev,
+        nameToSet: name,
+        valueToSet: value,
+        idToSet: id,
+      })
+    );
   }
 
   function setNewData({ currState, nameToSet, valueToSet, idToSet = 0 }) {
     const newData = [...currState];
-    const entryId = idToSet
-      ? newData.findIndex((section) => section.key === idToSet)
-      : 0;
+    const entryIds = getSectionsIndices(newData, idToSet);
+    if (entryIds === undefined) console.log("Couldn't find the id");
     const updatedData = {
-      ...newData[entryId],
+      ...newData[entryIds[0]].sectionCards[entryIds[1]],
       [nameToSet]: valueToSet,
     };
-
-    newData[entryId] = updatedData;
+    newData[entryIds[0]].sectionCards[entryIds[1]] = updatedData;
 
     return newData;
   }
 
+  function getSectionsIndices(data, id) {
+    for (let i = 0; i < data.length; i++) {
+      let index = data[i].sectionCards.findIndex(
+        (section) => section.key === id
+      );
+      if (index > -1) {
+        return [i, index];
+      }
+    }
+  }
+
   function handleNext() {
-    setSectionId(sectionId + 1);
+    setSectionId((prev) => prev + 1);
   }
 
   function handlePrevious() {
-    setSectionId(sectionId - 1);
+    setSectionId((prev) => prev - 1);
   }
 
   function handleAddSection() {
-    if (sectionId === 1) {
-      setEducationData((prev) => {
-        return [
-          ...prev,
-          {
-            key: crypto.randomUUID(),
-            school: "",
-            degree: "",
-            startDate: "",
-            endDate: "",
-            isCurrent: false,
-            city: "",
-            description: "",
-          },
-        ];
+    setInputData((prev) => {
+      const newData = [...prev];
+      const keys = Object.keys(initialDataCopy[sectionId].sectionCards[0]);
+      const newSection = getNewSectionCard(keys);
+      const updatedData = [...newData[sectionId].sectionCards, newSection];
+      newData[sectionId] = {
+        ...newData[sectionId],
+        sectionCards: updatedData,
+      };
+      return newData;
+    });
+  }
+
+  function handleClearAllData() {
+    const oldData = [...initialDataCopy];
+    const newData = [];
+    oldData.forEach((section, index) => {
+      let keys = Object.keys(section.sectionCards[0]);
+      let newSection = {
+        order: initialDataCopy[index].order,
+        sectionCards: [getNewSectionCard(keys)],
+      };
+      newData.push(newSection);
+    });
+
+    setInputData(newData);
+  }
+
+  function getNewSectionCard(keys) {
+    const sectionCard = {};
+    keys.forEach((name) => {
+      if (name === "key") {
+        sectionCard[name] = crypto.randomUUID();
+        return;
+      }
+      if (name === "isCurrent") {
+        sectionCard[name] = false;
+        return;
+      }
+      if (name === "level") {
+        sectionCard[name] = "none";
+        return;
+      }
+      sectionCard[name] = "";
+    });
+    return sectionCard;
+  }
+
+  function handleDeleteSection(e) {
+    const id = e.currentTarget.getAttribute("data-objid");
+    setInputData((prev) => {
+      const newData = [...prev];
+      const indices = getSectionsIndices(newData, id);
+      if (indices === undefined) return newData;
+      newData[indices[0]].sectionCards = [
+        ...newData[indices[0]].sectionCards.slice(0, indices[1]),
+        ...newData[indices[0]].sectionCards.slice(indices[1] + 1),
+      ];
+
+      return newData;
+    });
+  }
+
+  function handleClearSpecificData(e) {
+    const id = e.currentTarget.getAttribute("data-objid");
+    setInputData((prev) => {
+      const newData = [...prev];
+      const indices = getSectionsIndices(newData, id);
+      if (indices === undefined) return newData;
+      const keys = Object.keys(newData[indices[0]].sectionCards[indices[1]]);
+      const newSection = getNewSectionCard(keys);
+      newData[indices[0]] = {
+        ...newData[indices[0]],
+        sectionCards: [...newData[indices[0]].sectionCards],
+      };
+      newData[indices[0]].sectionCards[indices[1]] = {
+        ...newSection,
+      };
+
+      return newData;
+    });
+  }
+
+  function handleViewChange() {
+    setIsEdit((prev) => !prev);
+  }
+
+  function handleGoTo(e) {
+    if (e.target.nodeName !== "H1") return;
+    const id = parseInt(e.currentTarget.getAttribute("data-sectionid"));
+    setSectionId(id);
+  }
+
+  function handleDragStart(e) {
+    setActiveId(parseInt(e.active.id));
+  }
+
+  function handleDragEnd(e) {
+    const { over } = e;
+    if (!over || activeId === over.id) return;
+
+    const oldIndex = parseInt(activeId);
+    const newIndex = parseInt(over.id);
+
+    setInputData((prev) => {
+      const newData = [...prev];
+      const oldOrder = getSectionOrder([...prev]);
+      const oldOrderIndex = oldOrder.indexOf(oldIndex);
+      const newOrderIndex = oldOrder.indexOf(newIndex);
+
+      const reorderedSectionIds = arrayMove(
+        oldOrder,
+        oldOrderIndex,
+        newOrderIndex
+      );
+
+      reorderedSectionIds.forEach((sectionIndex, orderPosition) => {
+        newData[sectionIndex] = {
+          ...newData[sectionIndex],
+          order: orderPosition,
+        };
       });
-    } else if (sectionId === 2) {
-      setEmploymentData((prev) => {
-        return [
-          ...prev,
-          {
-            key: crypto.randomUUID(),
-            jobTitle: "",
-            employer: "",
-            startDate: "",
-            endDate: "",
-            isCurrent: false,
-            city: "",
-            description: "",
-          },
-        ];
-      });
+
+      return newData;
+    });
+  }
+
+  function getSectionOrder(data) {
+    const arr = [];
+    for (let i = 0; i < data.length; i++) {
+      let foundIndex = data.findIndex((obj) => obj.order === i);
+      if (foundIndex === -1) console.log("no index found");
+      arr.push(foundIndex);
     }
+    return arr;
   }
 
   return (
     <>
-      <SectionsContainer
-        onChange={handleChange}
-        onAdd={handleAddSection}
-        onPrev={handlePrevious}
-        onNext={handleNext}
-        personalData={personalData}
-        educationData={educationData}
-        employmentData={employmentData}
-        sectionId={sectionId}
-      />
+      <header>
+        <div className="header-buttons-container">
+          <button onClick={handleClearAllData} title="Clear all data">
+            Clear all
+          </button>
+          {
+            <button onClick={handleViewChange} title="Toggle full preview mode">
+              {isEdit ? "Full Preview" : "Edit"}
+            </button>
+          }
+        </div>
+      </header>
+      <main>
+        {isEdit && (
+          <CvEditor
+            onChange={handleChange}
+            onAdd={handleAddSection}
+            onPrev={handlePrevious}
+            onNext={handleNext}
+            onDelete={handleDeleteSection}
+            onClear={handleClearSpecificData}
+            inputData={inputData[sectionId].sectionCards}
+            sectionId={sectionId}
+          />
+        )}
+        <DndContext
+          onDragStart={handleDragStart}
+          onDragEnd={handleDragEnd}
+          collisionDetection={closestCorners}
+        >
+          <CvPreview
+            inputData={inputData}
+            sectionOrder={getSectionOrder(inputData)}
+            isEdit={isEdit}
+            onGoTo={handleGoTo}
+            activeId={activeId}
+          />
+        </DndContext>
+      </main>
     </>
   );
 }
