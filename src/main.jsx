76f305fb3478ab7cda10f87ec737ab52440fe70f
resq-u <jakub12lepianka@wp.pl>
2025-11-22 22:03:59 +0100

    Update imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "normalize.css";
+import "./styles/layout.css";
+import "./styles/cvEditor.css";
+import "./styles/inputCard.css";
+import "./styles/cvContainer.css";
+import "./styles/inputFields.css";
+import "./styles/mediaQueries.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
